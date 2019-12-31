fd92c33ed8d8a77d210a122afef83d3a180c874e
Jian He <He.Jian@ufh.com.cn>
2019-12-31 17:20:49 +0800

    Create OleDb and Sql Helper, Update OdbcHelper.

--- Code/Solution_Neytlds.Common/Neytlds.Common/DB/OleDbHelper.cs
+++ Code/Solution_Neytlds.Common/Neytlds.Common/DB/OleDbHelper.cs
@@ -2,19 +2,19 @@
 using System.Collections;
 using System.Configuration;
 using System.Data;
-using System.Data.Odbc;
+using System.Data.OleDb;
 
 namespace Neytlds.Common.DB
 {
     /// <summary>
-    /// 使用 Odbc 方式操作数据库
+    /// 使用 OleDb 方式操作数据库
     /// </summary>
-    public static class OdbcHelper
+    public static class OleDbHelper
     {
         /// <summary>
         /// 数据库连接字符串
         /// </summary>
-        public static string ConnectionString { get; set; } = ConfigurationManager.ConnectionStrings["OdbcConnectionString"]?.ConnectionString;
+        public static string ConnectionString { get; set; } = ConfigurationManager.ConnectionStrings["OleDbConnectionString"]?.ConnectionString;
 
         /// <summary>
         /// 存储缓存参数
@@ -29,11 +29,11 @@ namespace Neytlds.Common.DB
         /// <param name="comText">存储过程或 SQL 语句</param>
         /// <param name="commandParameter">用于执行命令的参数列表</param>
         /// <returns>影响的行数</returns>
-        public static int ExecuteNonQuery(string connectionString, CommandType comType, string comText, params OdbcParameter[] commandParameter)
+        public static int ExecuteNonQuery(string connectionString, CommandType comType, string comText, params OleDbParameter[] commandParameter)
         {
-            var cmd = new OdbcCommand();
+            var cmd = new OleDbCommand();
 
-            using var con = new OdbcConnection(connectionString);
+            using var con = new OleDbConnection(connectionString);
             PrepareCommand(cmd, con, null, comType, comText, commandParameter);
             int val = cmd.ExecuteNonQuery();
             cmd.Parameters.Clear();
@@ -48,9 +48,9 @@ namespace Neytlds.Common.DB
         /// <param name="comText">存储过程或 SQL 语句</param>
         /// <param name="commandParameter">用于执行命令的参数列表</param>
         /// <returns>影响的行数</returns>
-        public static int ExecuteNonQuery(OdbcConnection connection, CommandType comType, string comText, params OdbcParameter[] commandParameter)
+        public static int ExecuteNonQuery(OleDbConnection connection, CommandType comType, string comText, params OleDbParameter[] commandParameter)
         {
-            var cmd = new OdbcCommand();
+            var cmd = new OleDbCommand();
 
             PrepareCommand(cmd, connection, null, comType, comText, commandParameter);
             int val = cmd.ExecuteNonQuery();
@@ -66,9 +66,9 @@ namespace Neytlds.Common.DB
         /// <param name="comText">存储过程或 SQL 语句</param>
         /// <param name="commandParameter">用于执行命令的参数列表</param>
         /// <returns>影响的行数</returns>
-        public static int ExecuteNonQuery(OdbcTransaction trans, CommandType comType, string comText, params OdbcParameter[] commandParameter)
+        public static int ExecuteNonQuery(OleDbTransaction trans, CommandType comType, string comText, params OleDbParameter[] commandParameter)
         {
-            var cmd = new OdbcCommand();
+            var cmd = new OleDbCommand();
 
             PrepareCommand(cmd, trans.Connection, trans, comType, comText, commandParameter);
             int val = cmd.ExecuteNonQuery();
@@ -84,15 +84,15 @@ namespace Neytlds.Common.DB
         /// <param name="cmdText">存储过程或 SQL 语句</param>
         /// <param name="commandParameter">用于执行命令的参数列表</param>
         /// <returns>查询结果</returns>
-        public static OdbcDataReader ExecuteReader(string connectionString, CommandType cmdType, string cmdText, params OdbcParameter[] commandParameter)
+        public static OleDbDataReader ExecuteReader(string connectionString, CommandType cmdType, string cmdText, params OleDbParameter[] commandParameter)
         {
-            var cmd = new OdbcCommand();
-            var conn = new OdbcConnection(connectionString);
+            var cmd = new OleDbCommand();
+            var conn = new OleDbConnection(connectionString);
 
             try
             {
                 PrepareCommand(cmd, conn, null, cmdType, cmdText, commandParameter);
-                OdbcDataReader rdr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                OleDbDataReader rdr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
                 cmd.Parameters.Clear();
                 return rdr;
             }
@@ -111,11 +111,11 @@ namespace Neytlds.Common.DB
         /// <param name="comText">存储过程或 SQL 语句</param>
         /// <param name="commandParameter">用于执行命令的参数列表</param>
         /// <returns>查询结果的第一行第一列</returns>
-        public static object ExecuteScalar(string connectionString, CommandType comType, string comText, params OdbcParameter[] commandParameter)
+        public static object ExecuteScalar(string connectionString, CommandType comType, string comText, params OleDbParameter[] commandParameter)
         {
-            var cmd = new OdbcCommand();
+            var cmd = new OleDbCommand();
 
-            using var connection = new OdbcConnection(connectionString);
+            using var connection = new OleDbConnection(connectionString);
             PrepareCommand(cmd, connection, null, comType, comText, commandParameter);
             var val = cmd.ExecuteScalar();
             cmd.Parameters.Clear();
@@ -130,9 +130,9 @@ namespace Neytlds.Common.DB
         /// <param name="comText">存储过程或 SQL 语句</param>
         /// <param name="commandParameter">用于执行命令的参数列表</param>
         /// <returns>查询结果的第一行第一列</returns>
-        public static object ExecuteScalar(OdbcConnection connection, CommandType comType, string comText, params OdbcParameter[] commandParameter)
+        public static object ExecuteScalar(OleDbConnection connection, CommandType comType, string comText, params OleDbParameter[] commandParameter)
         {
-            var cmd = new OdbcCommand();
+            var cmd = new OleDbCommand();
 
             PrepareCommand(cmd, connection, null, comType, comText, commandParameter);
             object val = cmd.ExecuteScalar();
@@ -145,8 +145,8 @@ namespace Neytlds.Common.DB
         /// 将参数数组添加到缓存中
         /// </summary>
         /// <param name="cacheKey">缓存参数的 key 值</param>
-        /// <param name="cmdParms">要缓存的 OdbcParameter 数组</param>
-        public static void CacheParameters(string cacheKey, params OdbcParameter[] commandParameter)
+        /// <param name="cmdParms">要缓存的 OleDbParameter 数组</param>
+        public static void CacheParameters(string cacheKey, params OleDbParameter[] commandParameter)
         {
             parmCache[cacheKey] = commandParameter;
         }
@@ -155,18 +155,18 @@ namespace Neytlds.Common.DB
         /// 检索缓存参数
         /// </summary>
         /// <param name="cacheKey">用于查找参数的 key 值</param>
-        /// <returns>缓存在 OdbcParameter 的数组</returns>
-        public static OdbcParameter[] GetCachedParameters(string cacheKey)
+        /// <returns>缓存在 OleDbParameter 的数组</returns>
+        public static OleDbParameter[] GetCachedParameters(string cacheKey)
         {
-            var cachedParms = (OdbcParameter[])parmCache[cacheKey];
+            var cachedParms = (OleDbParameter[])parmCache[cacheKey];
 
             if (cachedParms == null) { return null; }
 
-            var clonedParms = new OdbcParameter[cachedParms.Length];
+            var clonedParms = new OleDbParameter[cachedParms.Length];
 
             for (int i = 0, j = cachedParms.Length; i < j; i++)
             {
-                clonedParms[i] = (OdbcParameter)((ICloneable)cachedParms[i]).Clone();
+                clonedParms[i] = (OleDbParameter)((ICloneable)cachedParms[i]).Clone();
             }
 
             return clonedParms;
@@ -184,7 +184,7 @@ namespace Neytlds.Common.DB
         /// <param name="cmdType">指定如何解释字符串</param>
         /// <param name="cmdText">存储过程或 SQL 语句</param>
         /// <param name="cmdParms">用于执行命令的参数列表</param>
-        private static void PrepareCommand(OdbcCommand cmd, OdbcConnection conn, OdbcTransaction trans, CommandType comType, string comText, OdbcParameter[] cmdParms)
+        private static void PrepareCommand(OleDbCommand cmd, OleDbConnection conn, OleDbTransaction trans, CommandType comType, string comText, OleDbParameter[] cmdParms)
         {
             if (conn.State != ConnectionState.Open) { conn.Open(); }
 
@@ -197,7 +197,7 @@ namespace Neytlds.Common.DB
 
             if (cmdParms != null)
             {
-                foreach (OdbcParameter parm in cmdParms)
+                foreach (OleDbParameter parm in cmdParms)
                 {
                     cmd.Parameters.Add(parm);
                 }
--- Code/Solution_Neytlds.Common/Neytlds.Common/DB/SqlHelper.cs
+++ Code/Solution_Neytlds.Common/Neytlds.Common/DB/SqlHelper.cs
@@ -2,19 +2,20 @@
 using System.Collections;
 using System.Configuration;
 using System.Data;
-using System.Data.Odbc;
+using System.Data.Sql;
+using System.Data.SqlClient;
 
 namespace Neytlds.Common.DB
 {
     /// <summary>
-    /// 使用 Odbc 方式操作数据库
+    /// 使用 Sql 方式操作数据库
     /// </summary>
-    public static class OdbcHelper
+    public static class SqlHelper
     {
         /// <summary>
         /// 数据库连接字符串
         /// </summary>
-        public static string ConnectionString { get; set; } = ConfigurationManager.ConnectionStrings["OdbcConnectionString"]?.ConnectionString;
+        public static string ConnectionString { get; set; } = ConfigurationManager.ConnectionStrings["SqlConnectionString"]?.ConnectionString;
 
         /// <summary>
         /// 存储缓存参数
@@ -29,11 +30,11 @@ namespace Neytlds.Common.DB
         /// <param name="comText">存储过程或 SQL 语句</param>
         /// <param name="commandParameter">用于执行命令的参数列表</param>
         /// <returns>影响的行数</returns>
-        public static int ExecuteNonQuery(string connectionString, CommandType comType, string comText, params OdbcParameter[] commandParameter)
+        public static int ExecuteNonQuery(string connectionString, CommandType comType, string comText, params SqlParameter[] commandParameter)
         {
-            var cmd = new OdbcCommand();
+            var cmd = new SqlCommand();
 
-            using var con = new OdbcConnection(connectionString);
+            using var con = new SqlConnection(connectionString);
             PrepareCommand(cmd, con, null, comType, comText, commandParameter);
             int val = cmd.ExecuteNonQuery();
             cmd.Parameters.Clear();
@@ -48,9 +49,9 @@ namespace Neytlds.Common.DB
         /// <param name="comText">存储过程或 SQL 语句</param>
         /// <param name="commandParameter">用于执行命令的参数列表</param>
         /// <returns>影响的行数</returns>
-        public static int ExecuteNonQuery(OdbcConnection connection, CommandType comType, string comText, params OdbcParameter[] commandParameter)
+        public static int ExecuteNonQuery(SqlConnection connection, CommandType comType, string comText, params SqlParameter[] commandParameter)
         {
-            var cmd = new OdbcCommand();
+            var cmd = new SqlCommand();
 
             PrepareCommand(cmd, connection, null, comType, comText, commandParameter);
             int val = cmd.ExecuteNonQuery();
@@ -66,9 +67,9 @@ namespace Neytlds.Common.DB
         /// <param name="comText">存储过程或 SQL 语句</param>
         /// <param name="commandParameter">用于执行命令的参数列表</param>
         /// <returns>影响的行数</returns>
-        public static int ExecuteNonQuery(OdbcTransaction trans, CommandType comType, string comText, params OdbcParameter[] commandParameter)
+        public static int ExecuteNonQuery(SqlTransaction trans, CommandType comType, string comText, params SqlParameter[] commandParameter)
         {
-            var cmd = new OdbcCommand();
+            var cmd = new SqlCommand();
 
             PrepareCommand(cmd, trans.Connection, trans, comType, comText, commandParameter);
             int val = cmd.ExecuteNonQuery();
@@ -84,15 +85,15 @@ namespace Neytlds.Common.DB
         /// <param name="cmdText">存储过程或 SQL 语句</param>
         /// <param name="commandParameter">用于执行命令的参数列表</param>
         /// <returns>查询结果</returns>
-        public static OdbcDataReader ExecuteReader(string connectionString, CommandType cmdType, string cmdText, params OdbcParameter[] commandParameter)
+        public static SqlDataReader ExecuteReader(string connectionString, CommandType cmdType, string cmdText, params SqlParameter[] commandParameter)
         {
-            var cmd = new OdbcCommand();
-            var conn = new OdbcConnection(connectionString);
+            var cmd = new SqlCommand();
+            var conn = new SqlConnection(connectionString);
 
             try
             {
                 PrepareCommand(cmd, conn, null, cmdType, cmdText, commandParameter);
-                OdbcDataReader rdr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                SqlDataReader rdr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
                 cmd.Parameters.Clear();
                 return rdr;
             }
@@ -111,11 +112,11 @@ namespace Neytlds.Common.DB
         /// <param name="comText">存储过程或 SQL 语句</param>
         /// <param name="commandParameter">用于执行命令的参数列表</param>
         /// <returns>查询结果的第一行第一列</returns>
-        public static object ExecuteScalar(string connectionString, CommandType comType, string comText, params OdbcParameter[] commandParameter)
+        public static object ExecuteScalar(string connectionString, CommandType comType, string comText, params SqlParameter[] commandParameter)
         {
-            var cmd = new OdbcCommand();
+            var cmd = new SqlCommand();
 
-            using var connection = new OdbcConnection(connectionString);
+            using var connection = new SqlConnection(connectionString);
             PrepareCommand(cmd, connection, null, comType, comText, commandParameter);
             var val = cmd.ExecuteScalar();
             cmd.Parameters.Clear();
@@ -130,9 +131,9 @@ namespace Neytlds.Common.DB
         /// <param name="comText">存储过程或 SQL 语句</param>
         /// <param name="commandParameter">用于执行命令的参数列表</param>
         /// <returns>查询结果的第一行第一列</returns>
-        public static object ExecuteScalar(OdbcConnection connection, CommandType comType, string comText, params OdbcParameter[] commandParameter)
+        public static object ExecuteScalar(SqlConnection connection, CommandType comType, string comText, params SqlParameter[] commandParameter)
         {
-            var cmd = new OdbcCommand();
+            var cmd = new SqlCommand();
 
             PrepareCommand(cmd, connection, null, comType, comText, commandParameter);
             object val = cmd.ExecuteScalar();
@@ -145,8 +146,8 @@ namespace Neytlds.Common.DB
         /// 将参数数组添加到缓存中
         /// </summary>
         /// <param name="cacheKey">缓存参数的 key 值</param>
-        /// <param name="cmdParms">要缓存的 OdbcParameter 数组</param>
-        public static void CacheParameters(string cacheKey, params OdbcParameter[] commandParameter)
+        /// <param name="cmdParms">要缓存的 SqlParameter 数组</param>
+        public static void CacheParameters(string cacheKey, params SqlParameter[] commandParameter)
         {
             parmCache[cacheKey] = commandParameter;
         }
@@ -155,18 +156,18 @@ namespace Neytlds.Common.DB
         /// 检索缓存参数
         /// </summary>
         /// <param name="cacheKey">用于查找参数的 key 值</param>
-        /// <returns>缓存在 OdbcParameter 的数组</returns>
-        public static OdbcParameter[] GetCachedParameters(string cacheKey)
+        /// <returns>缓存在 SqlParameter 的数组</returns>
+        public static SqlParameter[] GetCachedParameters(string cacheKey)
         {
-            var cachedParms = (OdbcParameter[])parmCache[cacheKey];
+            var cachedParms = (SqlParameter[])parmCache[cacheKey];
 
             if (cachedParms == null) { return null; }
 
-            var clonedParms = new OdbcParameter[cachedParms.Length];
+            var clonedParms = new SqlParameter[cachedParms.Length];
 
             for (int i = 0, j = cachedParms.Length; i < j; i++)
             {
-                clonedParms[i] = (OdbcParameter)((ICloneable)cachedParms[i]).Clone();
+                clonedParms[i] = (SqlParameter)((ICloneable)cachedParms[i]).Clone();
             }
 
             return clonedParms;
@@ -184,7 +185,7 @@ namespace Neytlds.Common.DB
         /// <param name="cmdType">指定如何解释字符串</param>
         /// <param name="cmdText">存储过程或 SQL 语句</param>
         /// <param name="cmdParms">用于执行命令的参数列表</param>
-        private static void PrepareCommand(OdbcCommand cmd, OdbcConnection conn, OdbcTransaction trans, CommandType comType, string comText, OdbcParameter[] cmdParms)
+        private static void PrepareCommand(SqlCommand cmd, SqlConnection conn, SqlTransaction trans, CommandType comType, string comText, SqlParameter[] cmdParms)
         {
             if (conn.State != ConnectionState.Open) { conn.Open(); }
 
@@ -197,7 +198,7 @@ namespace Neytlds.Common.DB
 
             if (cmdParms != null)
             {
-                foreach (OdbcParameter parm in cmdParms)
+                foreach (SqlParameter parm in cmdParms)
                 {
                     cmd.Parameters.Add(parm);
                 }
